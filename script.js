ac73401155db835ac074a6b23480a1ec46c767c2
Duc_hoc_code <100946720+duccodedao@users.noreply.github.com>
2024-04-26 19:00:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // Hiển thị thông báo ưu đãi khi trang được tải
   Swal.fire({
       title: 'Ưu đãi đặc biệt!',
-      html: '<img src="img/sale/sale20.png" style="width: 100%;" /><br><br>Chúng tôi có một ưu đãi đặc biệt dành cho bạn. Hãy ghé thăm cửa hàng của chúng tôi ngay!',
+      html: '<img src="img/sale/20sale.png" style="width: 100%;" /><br><br>Chúng tôi có một ưu đãi đặc biệt dành cho bạn. Hãy ghé thăm cửa hàng của chúng tôi ngay!',
       confirmButtonText: 'Đồng ý'
   });
 });
